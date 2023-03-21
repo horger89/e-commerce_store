fbe69ca3967a2d73f2f15f459f4575e58ca4ece9
Gergo Horvath <111783205+horger89@users.noreply.github.com>
2023-03-21 22:32:52 +0000

    worked on cart functions

--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from . models import Product
 from category.models import Category
+from carts.views import _cart_id
+from carts.models import CartItem
 
 # Create your views here.
 
@@ -29,12 +31,14 @@ def store(request, category_slug = None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug = category_slug, slug = product_slug)
-
+        in_cart = CartItem.objects.filter(cart__cart_id = _cart_id(request), product = single_product).exists()  # cart is a foreign key in CartItem model and this is how we acces cart_id in Cart model
+        
     except Exception as e:
         raise e 
     
     context = {
         'single_product' : single_product,
+        'in_cart' : in_cart,
     }
 
     return render(request, 'store/product_detail.html', context)
