32047f0304920a116ef32a36424cbb8bb7bdd971
Gergo Horvath <111783205+horger89@users.noreply.github.com>
2023-04-10 21:48:56 +0100

    finished account verification

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from . forms import RegistrationForm
 from . models import Account
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
 # Email verification
 from django.contrib.sites.shortcuts import get_current_site
@@ -41,8 +42,9 @@ def register(request):
             send_email.send()
             
 
-            messages.success(request, 'Registration succesfull.')
-            return redirect('register')
+            #messages.success(request, 'Thank you for registering. Please check your emails (including spams) to activate your account.')
+            #return redirect('register')
+            return redirect('/accounts/login/?command=verification&email='+email)
 
     else:
         form = RegistrationForm()
@@ -61,8 +63,8 @@ def login(request):
         user = auth.authenticate(email=email, password=password)
         if user is not None:
             auth.login(request,user)
-            #messages.success(request,'You are now logged in')
-            return redirect('home')
+            messages.success(request,'You are now logged in')
+            return redirect('dashboard')
         else:
             messages.error(request,'Invalid login details')
             return redirect('login')
@@ -78,5 +80,24 @@ def logout(request):
 
 
 
-def activate(request):
-    return
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode() # gives pk of user
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError,Account.DoesNotExist):
+        user = None
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Congratulations, Your account is avtivated')
+        return redirect('login')
+
+    else:
+        messages.error(request, 'Invalid activation link')
+        return redirect('register')
+
+ 
+
+@login_required(login_url = 'login')
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
