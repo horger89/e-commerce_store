611a764b72ff39b9bce3c53052f5034e6b935517
Gergo Horvath <111783205+horger89@users.noreply.github.com>
2023-04-22 23:23:28 +0100

    worked on checkout view and form

--- orders/forms.py
+++ orders/forms.py
@@ -1,7 +1,11 @@
 from django import forms
-from .models import Order
+from . models import Order
 
 class OrderForm(forms.ModelForm):
+
+    address_line_2 = forms.CharField(required=False)
+    order_note = forms.CharField(required=False)
+
     class Meta:
         model = Order
-        fields = ['first_name', 'last_name', 'phone', 'email', 'address_line_1', 'address_line_2', 'country', 'stae', 'city', 'oreder_note'] 
+        fields = ['first_name', 'last_name', 'phone', 'email', 'address_line_1', 'address_line_2', 'country', 'state', 'city', 'order_note'] 
--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,11 @@
 from carts.models import CartItem
 from django.shortcuts import redirect, render
+from .forms import OrderForm
+from .models import Order
+import datetime
 
 # Create your views here.
-def place_order(request):
+def place_order(request, total=0, quantity=0,grand_total=0,tax=0):
     current_user = request.user
 
     #if the cart is less then or equal to 0, then redirect back to shop
@@ -11,5 +14,54 @@ def place_order(request):
     if cart_count <= 0:
         return redirect('store')
     
+    
+    tax= 0
+    grand_total= 0
+    
+    for cart_item in cart_items:
+        total += (cart_item.product.price * cart_item.quantity)
+        quantity += cart_item.quantity
+    
+    tax = (5 * total)/100
+    grand_total = total + tax
+    print(grand_total)
+    
+
     if request.method == 'POST':
         form = OrderForm(request.POST)
+        if form.is_valid():
+            print('it is valid')
+            #store all billing information inside order table
+            data = Order()
+            data.user = current_user
+            data.first_name = form.cleaned_data.get('first_name')
+            data.last_name = form.cleaned_data.get('last_name')
+            data.phone = form.cleaned_data.get('phone')
+            data.email = form.cleaned_data.get('email')
+            data.address_line_1 = form.cleaned_data.get('address_line_1')
+            data.address_line_2 = form.cleaned_data.get('address_line_2')
+            data.country = form.cleaned_data.get('country')
+            data.state = form.cleaned_data.get('state')
+            data.city = form.cleaned_data.get('city')
+            data.order_note = form.cleaned_data.get('order_note')
+            data.order_total = grand_total
+            data.tax = tax
+            data.ip = request.META.get('REMOTE_ADDR') # fetch user ip
+            
+            data.save()
+            # Generate order ID
+            yr = int(datetime.date.today().strftime('%Y'))
+            dt = int(datetime.date.today().strftime('%d'))
+            mt = int(datetime.date.today().strftime('%m'))
+            d = datetime.date(yr, mt, dt)
+            current_date = d.strftime('%Y%m%d')
+            order_number = current_date + str(data.id)
+            data.order_number = order_number
+            data.save()
+            return redirect('checkout')
+        
+        else:
+            print('went to else')
+            return redirect('checkout')
+            
+
